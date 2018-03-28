19a33f201af0575b3b8c169ed065c5ca753524cd
Thorto <theotimdelan@hotmail.fr>
2018-03-28 13:47:01 +0200

    Commit pour push test

--- ChessApp/src/chessapp/ChessApp.java
+++ ChessApp/src/chessapp/ChessApp.java
@@ -5,6 +5,10 @@
  */
 package chessapp;
 
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author aymeric
@@ -16,6 +20,27 @@ public class ChessApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ceci est mon premier programme");
+        ChessApp checkersFrame = new ChessApp();
+        checkersFrame.go();
+    }
+    
+    public void go(){
+        System.out.println("invocation de la fonction go()");
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setTitle("bonsoir à tous");
+        frame.setSize(400,400);
+        JPanel checkersBoard;
+        //checkersBoard = setCheckerboard(10);
+        //frame.add(checkersBoard);
+    }
+    
+    public JPanel setCheckerBoard(JPanel checkersBoard){
+        GridLayout layout = new GridLayout();
+        checkersBoard = new JPanel(layout);
+        
+        return checkersBoard;
     }
     
 }
