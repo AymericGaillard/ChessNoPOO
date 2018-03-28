d5b174d59bb2b808b9bfc1e248f067f6366f3636
Thorto <theotimdelan@hotmail.fr>
2018-03-28 14:45:05 +0200

    Exercice 1

--- ChessApp/src/chessapp/ChessApp.java
+++ ChessApp/src/chessapp/ChessApp.java
@@ -5,6 +5,7 @@
  */
 package chessapp;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,19 +29,43 @@ public class ChessApp {
     public void go(){
         System.out.println("invocation de la fonction go()");
         JFrame frame = new JFrame();
+        JPanel checkersBoard;
+        checkersBoard = setCheckerBoard(10);
+        frame.add(checkersBoard);
+        frame.pack();
         frame.setVisible(true);
         frame.setTitle("bonsoir à tous");
         frame.setSize(400,400);
-        JPanel checkersBoard;
-        //checkersBoard = setCheckerboard(10);
-        //frame.add(checkersBoard);
     }
     
-    public JPanel setCheckerBoard(JPanel checkersBoard){
-        GridLayout layout = new GridLayout();
-        checkersBoard = new JPanel(layout);
-        
+    public JPanel setCheckerBoard(int n){
+        GridLayout layout = new GridLayout(n,n);
+        JPanel checkersBoard = new JPanel();
+        checkersBoard.setLayout(layout);
+        JPanel cellule;
+        JPanel thing;
+        for(int j=0; j<n;j++){
+            for(int i=0;i<n;i++){
+                cellule = new JPanel();
+                if((i+j)%2==0){
+                    cellule.setBackground(Color.white);
+                }
+                else{
+                    cellule.setBackground(Color.black);
+                    if(j<3){
+                        thing = new JPanel();
+                        thing.setBackground(Color.red);
+                        cellule.add(thing);
+                    }
+                    else if(j>6){
+                        thing = new JPanel();
+                        thing.setBackground(Color.blue);
+                        cellule.add(thing);
+                    }
+                }
+                checkersBoard.add(cellule);
+            } 
+        }
         return checkersBoard;
     }
-    
 }
